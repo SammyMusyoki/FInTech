e997225ba3b9cdb82192f30512769d19db256774
SammyMusyoki <sammerkhan328@gmail.com>
2024-04-06 11:05:28 +0300

    few changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Link, Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
+import { TouchableOpacity } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -13,13 +17,15 @@ export {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const InitialLayout = () => {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  const router = useRouter()
+
+
   useEffect(() => {
     if (error) throw error;
   }, [error]);
@@ -34,23 +40,57 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-
   return (
       <Stack>
-        <Stack.Screen name='index' options={{ headerShown: false}} />
-        {/* For SignUp page header */}
-        <Stack.Screen name='signup'
-        options={{
-          title: '',
-          headerBackTitle: '',
-          headerShadowVisible: false,
-          headerStyle: { backgroundColor: Colors.background }
-        }}
-        />
-      </Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="signup"
+              options={{
+                title: '',
+                headerBackTitle: '',
+                headerShadowVisible: false,
+                headerStyle: { backgroundColor: Colors.background },
+                headerLeft: () => (
+                  <TouchableOpacity onPress={router.back}>
+                    <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+                  </TouchableOpacity>
+                ),
+              }}
+            />
+
+            <Stack.Screen
+              name="login"
+              options={{
+                title: '',
+                headerBackTitle: '',
+                headerShadowVisible: false,
+                headerStyle: { backgroundColor: Colors.background },
+                headerLeft: () => (
+                  <TouchableOpacity onPress={router.back}>
+                    <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+                  </TouchableOpacity>
+                ),
+                headerRight: () => (
+                  <Link href={'/help'} asChild>
+                    <TouchableOpacity>
+                      <Ionicons name="help-circle-outline" size={34} color={Colors.dark} />
+                    </TouchableOpacity>
+                  </Link>
+                ),
+              }}
+            />
+    </Stack>
   );
 }
+
+const RootLayoutNav = () => {
+  return (
+      <GestureHandlerRootView style={{ flex: 1}}>
+        <StatusBar style='light'/>
+        <InitialLayout />; 
+      </GestureHandlerRootView>
+
+  )
+};
+
+export default RootLayoutNav
